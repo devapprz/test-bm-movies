8663e4ff7f7c95f19e751cbf3b9a21d23463e46f
yusufrizkyana <devapprz@gmail.com>
2022-10-12 16:37:38 +0700

    add entity for genre #6

--- app/src/main/java/com/yusuf/bankmandiri/movies/feature/genres/model/data/Genre.kt
+++ app/src/main/java/com/yusuf/bankmandiri/movies/feature/genres/model/data/Genre.kt
@@ -2,13 +2,17 @@ package com.yusuf.bankmandiri.movies.feature.genres.model.data
 
 import com.google.gson.annotations.Expose
 import androidx.annotation.Keep
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Keep
+@Entity(tableName = "genre")
 data class Genre(
     @SerializedName("id")
     @Expose
-    val id: Int? = null,
+    @PrimaryKey()
+    val id: Int = 0,
     @SerializedName("name")
     @Expose
     val name: String? = null
